74cecb1ce34acdc5495f337daf68ca05e2f63306
KwasiEvans <73921854+KwasiEvans@users.noreply.github.com>
2021-06-30 11:33:45 +0000

    HMS

---
+++ hospital/hms/include/footer.php
@@ -0,0 +1,10 @@
+<footer>
+				<div class="footer-inner">
+					<div class="pull-left">
+						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> HMS</span>. <span>Developed by CampCodes</span>
+					</div>
+					<div class="pull-right">
+						<span class="go-top"><i class="ti-angle-up"></i></span>
+					</div>
+				</div>
+			</footer>
